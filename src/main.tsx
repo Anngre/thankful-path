dc1b650ea050aa4a8da08387d9940e4a73f1f21a
Anngre <gren.anna.joanna@gmail.com>
2024-03-06 13:29:16 +0100

    Start the app with createBrowserRouter

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Login from "./components/Login";
+import Note from "./components/Note";
+import SignIn from "./components/SignIn";
+import ErrorPage from "./components/ErrorPage";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "notes/:noteId",
+        element: <Note />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
